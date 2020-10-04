8ec350fe12007c14449787a6908629c5fd2add79
marianatoscano <mariana.toscano.7@hotmail.com>
2020-10-04 17:01:54 +0100

    form2

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import SearchBar from './SearchBar'
-const App = () => {
-  return (
-  <div className="ui container" style={{marginTop: '10px'}}>
-    <SearchBar />
-  </div>
-  );
-};
+
+// const App = () => { substituimos pelo q ta em baixo
+class App extends React.Component {
+  onSearchSubmit(term) {
+    console.log(term);
+  }
+
+  render() {
+    return (
+    <div className="ui container" style={{marginTop: '10px'}}>
+      <SearchBar onSubmit={this.onSearchSubmit} /> 
+      {/* no onSubmit podiamos meter o q quisessemos, mas ele meteu on submit pq cada vez q run this function anyime the seacrh bar is submited, so on submit run this function  */}
+    </div>
+    );
+  }
+}
+
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,24 @@
 import React from 'react';
+
 class SearchBar extends React.Component {
   state = { term : ''};
+
+  onFormSubmit = event => {
+    event.preventDefault(); //o q previne o form de submeter o form automaticamente
+  
+    this.props.onSubmit(this.state.term);
+  };
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image Search</label>
             <input 
               type="text"
-              value={this.state.term}
-              onChange={e => this.setState({ term: e.target.value })}
+              value={this.state.term} // aquilo q metermos aqui é o que aparece na search bar
+              onChange={e => this.setState({ term: e.target.value })} // se quisesse meter tudo em uppercase era meter a frente do value toUpperCase()
             />
           </div>
         </form>
@@ -18,4 +26,5 @@ class SearchBar extends React.Component {
     );
   }
 }
+
 export default SearchBar;
