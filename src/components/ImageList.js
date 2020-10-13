257e319f4f47943291ea94cf712b496e317759c5
marianatoscano <mariana.toscano.7@hotmail.com>
2020-10-13 01:10:40 +0100

    DOM

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,13 @@
+import './ImageList.css';
 import React from 'react';
+import ImageCard from './ImageCard';
 
 const ImageList = (props) => {
   const images = props.images.map((image) => {
-    return <img src={image.urls.regular} /> //vamos ao console log, e ai conseguimos ver q as img q queremos tao nos urls e escolhemos um
+    return <ImageCard key={image.id} image={image} />; //vamos ao console log, e ai conseguimos ver q as img q queremos tao nos urls e escolhemos um
   });
 
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
